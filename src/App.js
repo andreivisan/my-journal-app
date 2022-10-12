4f684942e230b6713d871d580e12ced5467c41f0
Andrei Visan <andrei.visan@kpn.com>
2022-10-12 23:00:08 +0200

    Initial commit - My Journal React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <head>
+        <title>My Journal</title>
+      </head>
+      
+      <main>
+        <div className="min-h-screen">
+          <div className="text-center p-10 ">
+            <h1 className="text-5xl py-2 font-bold font-vollkorn">MyJournal</h1>
+          </div>
+          <div className="flex justify-center gap-16 font-vollkorn">
+            <a href="#">Entries</a>
+            <a href="#">Drafts</a>
+            <a href="#">New Posts</a>
+          </div>
+          <div className="py-20 font-mate">
+            <ul>
+              <li className="flex justify-center gap-52 py-2 text-teal-600">
+                <a href="#">7 Oct 2022</a>
+                <a href="#">12:00</a>
+              </li>
+              <li className="flex justify-center gap-52 py-2">
+                <a href="#">29 Jun 2022</a>
+                <a href="#">07:00</a>
+              </li>
+              <li className="flex justify-center gap-52 py-2">
+                <a href="#">16 May 2022</a>
+                <a href="#">00:16</a>
+              </li>
+              <li className="flex justify-center gap-52 py-2">
+                <a href="#">25 Apr 2022</a>
+                <a href="#">07:10</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
