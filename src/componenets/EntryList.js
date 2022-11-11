b230ad4b3f7c35f7127c9ae46021d2486f44f2cc
Andrei Visan <andrei.m.visan@gmail.com>
2022-11-11 15:13:26 +0100

    Temp commit

--- src/componenets/EntryList.js
+++ src/componenets/EntryList.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react'
 import EntryRow from './EntryRow';
 
 class EntryList extends Component {
-  render() {
-    return (
-        <div className="py-20 font-mate">
-            <ul>
-                <EntryRow />
+    constructor(props) {
+        super(props);
 
-                <EntryRow />
-            </ul>
-        </div>
-    )
-  }
+        this.state = {
+            entries: []
+        }
+
+        this.fetchEntries = this.fetchEntries.bind(this);
+    }
+
+    async fetchEntries() {
+        const response = await fetch('http://localhost:8080/entries');
+
+        if (!response.ok) {
+            throw new Error('Data coud not be fetched!');
+        } else {
+            this.setState({ entries: response.json() });
+        }
+    }
+
+    render() {
+        return (
+            <div className="py-20 font-mate">
+                <ul>
+                    <EntryRow />
+
+                    <EntryRow />
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default EntryList;
